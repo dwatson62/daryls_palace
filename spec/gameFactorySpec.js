c0ee0ace5724e4895882f11b4a9208304bda3fc5
Daryl Watson <darylwtsn@gmail.com>
2015-08-03 16:12:35 +0100

    Creates a deck of cards

--- spec/gameFactorySpec.js
+++ spec/gameFactorySpec.js
@@ -12,4 +12,9 @@ describe('Blackjack Factory', function() {
     expect(game).toBeDefined();
   });
 
+  it('each deck has 52 cards', function() {
+    console.log(game.deck)
+    expect(game.deck.length).toEqual(52);
+  });
+
 });
--- src/gameFactory.js
+++ src/gameFactory.js
@@ -1,23 +1,24 @@
 blackjackGame.factory('gameFactory', function() {
 
   var Game = function() {
+    this.deck = []
+    this.createDeck();
+  };
 
+  Game.prototype.createDeck = function() {
+    var suits = ["D", "H", "C", "S"];
+    for(x in suits) { this.createSuit(suits[x]); }
+  };
+
+  Game.prototype.createSuit = function(suit) {
+    for (x = 2; x < 11; x ++) {
+      number = x.toString();
+      this.deck.push(suit + number)
+    }
+    var faces = ["J", "Q", "K", "A"];
+    for (x in faces) { this.deck.push(suit + faces[x]); }
   };
 
   return Game;
 
 });
-
-// def createdeck(suit)
-//     for x in 2..10
-//         number = x.to_s
-//         $deck.push(suit + number)
-//     end
-//     faces = ["J","Q","K","A"]
-//     faces.each {|x| $deck.push(suit + x)}
-// end
-
-// suitarray = ["D", "H", "C", "S"]
-// suitarray.each { |suit| createdeck(suit)}
-
-
